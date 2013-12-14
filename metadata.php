4b2d9b1e37f67d46260db44551ba618ab87a11b3
localhost8080 <localhost8080@gmail.com>
2013-12-14 14:39:53 +0000

    changing a few function calls

--- metadata.php
+++ metadata.php
@@ -2,14 +2,15 @@
 <li class="pull-right">
 <?php
 echo '<span class="author">' . _e(' by ', 'multiloquent');
-echo '<a rel="author" href="' . home_url() . '/about">' . the_author() . '</a>';
+echo '<a rel="author" href="' . home_url() . '/about">' . get_the_author() . '</a>';
 echo '</span>';
-echo '<span class="divider">/</span><span class="day ' . the_time(__('M', 'multiloquent')) . '"> <span
-        class="fa fa-calendar fa-fw"></span> <time datetime="' . the_time('c') . '">' .
-     the_time(__('M jS, Y', 'multiloquent')) . '</time>';
+echo '<span class="divider">/</span>';
+echo '<span class="day ' . get_the_time(__('M', 'multiloquent')) . '"> <span
+        class="fa fa-calendar fa-fw"></span> <time datetime="' . get_the_time('c') . '">' .
+     get_the_time(__('M jS, Y', 'multiloquent')) . '</time>';
 if (get_the_time('c') != get_the_modified_time('c')) {
-    echo '<span class="fa fa-refresh fa-fw"></span> <time datetime="' . the_modified_time('c') . '">' .
-         the_modified_time(__('M jS, Y', 'multiloquent')) . '</time>';
+    echo '<span class="fa fa-refresh fa-fw"></span> <time datetime="' . get_the_modified_time('c') . '">' .
+         get_the_modified_time(__('M jS, Y', 'multiloquent')) . '</time>';
 }
 echo '</span>';
 edit_post_link();
