3c5b11dfe45439f24c61ac888058c9034e161da4
localhost8080 <localhost8080@gmail.com>
2013-12-14 00:26:07 +0000

    Signed-off-by: localhost8080 <localhost8080@gmail.com>

--- archive.php
+++ archive.php
@@ -6,41 +6,41 @@ if (have_posts()) {
 <div class="hero-unit">
     <div class="container">
         <header>
-			<?php /* If this is a category archive */ if (is_category()) { ?>
-			<h1 class="article_title"><?php printf( __('%s','multiloquent'), single_cat_title('', false)); ?></h1>
-			<?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
-			<h1 class="article_title">
-				<?php _e('Posts Tagged','multiloquent'); ?>
-				&#8216;
-				<?php single_tag_title(); ?>
-				&#8217;</h1>
-				<?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-				<h1 class="article_title"><?php printf( __('Archive for %s','multiloquent'), get_the_time(__('F jS, Y','multiloquent'))); ?></h1>
-				<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-				<h1 class="article_title"><?php printf( __('Archive for %s','multiloquent'), get_the_time(__('F Y','multiloquent'))); ?></h1>
-				<?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-				<h1 class="article_title"><?php printf( __('Archive for %s','multiloquent'), get_the_time('Y')); ?></h1>
-				<?php /* If this is a search */ } elseif (is_search()) { ?>
-				<h1 class="article_title">
-					<?php __('Search Results','multiloquent'); ?>
-				</h1>
-				<?php /* If this is an author archive */ } elseif (is_author()) { ?>
-				<h1 class="article_title">
-					<?php _e('All entries by this author','multiloquent'); ?>
-				</h1>
-				<?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-				<h1 class="article_title">
-					<?php _e('Blog Archives','multiloquent'); ?>
-				</h1>
-				<?php } ?>
-			</header>
+            <?php /* If this is a category archive */ if (is_category()) { ?>
+            <h1 class="article_title"><?php printf( __('%s','multiloquent'), single_cat_title('', false)); ?></h1>
+            <?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
+            <h1 class="article_title">
+                <?php _e('Posts Tagged','multiloquent'); ?>
+                &#8216;
+                <?php single_tag_title(); ?>
+                &#8217;</h1>
+                <?php /* If this is a daily archive */ } elseif (is_day()) { ?>
+                <h1 class="article_title"><?php printf( __('Archive for %s','multiloquent'), get_the_time(__('F jS, Y','multiloquent'))); ?></h1>
+                <?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
+                <h1 class="article_title"><?php printf( __('Archive for %s','multiloquent'), get_the_time(__('F Y','multiloquent'))); ?></h1>
+                <?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
+                <h1 class="article_title"><?php printf( __('Archive for %s','multiloquent'), get_the_time('Y')); ?></h1>
+                <?php /* If this is a search */ } elseif (is_search()) { ?>
+                <h1 class="article_title">
+                    <?php __('Search Results','multiloquent'); ?>
+                </h1>
+                <?php /* If this is an author archive */ } elseif (is_author()) { ?>
+                <h1 class="article_title">
+                    <?php _e('All entries by this author','multiloquent'); ?>
+                </h1>
+                <?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
+                <h1 class="article_title">
+                    <?php _e('Blog Archives','multiloquent'); ?>
+                </h1>
+                <?php } ?>
+        </header>
     </div>
 </div>
-<?php  	$post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
+<?php      $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
 <div class="container post">
     <div class="featurette">
         <section class="row">
-				<?php
+                <?php
     $tile_colour = get_random_blue_class();
     while (have_posts()) :
         the_post();
@@ -58,10 +58,8 @@ if (have_posts()) {
             $height = '500';
         }
         ?>
-				<div class="paralax_image_holder span4" style="margin-bottom: 30px;">
-                <img src="<?php echo $theimg?>" class="grayscale"
-                    alt="<?php the_title()?>"
-                    width="<?php echo $width ?>"
+                <div class="paralax_image_holder span4" style="margin-bottom: 30px;">
+                <img src="<?php echo $theimg?>" class="grayscale" alt="<?php the_title()?>" width="<?php echo $width ?>"
                     height="<?php echo $height ?>">
                 <div class="paralax_image_bg <?php echo $tile_colour?>"></div>
                 <div class="paralax_image_text">
@@ -69,7 +67,7 @@ if (have_posts()) {
                         <a href="<?php the_permalink() ?>"><?php  the_title()?></a>
                     </h1>
                     <p>
-							<?php
+                            <?php
         $posttags = wp_get_post_tags($post->ID);
         if ($posttags) {
             foreach ($posttags as $tag) {
@@ -80,27 +78,26 @@ if (have_posts()) {
             }
         }
         ?>
-						</p>
+                        </p>
                 </div>
             </div>
-			<?php endwhile; ?>
-		</section>
+            <?php endwhile; ?>
+        </section>
         <section>
-			<?php get_template_part('advert');?>
-		</section>
+            <?php get_template_part('advert');?>
+        </section>
         <nav class="navitems article white2">
-			<?php if(function_exists('pagenavi')) { pagenavi(); } ?>
-		</nav>
+            <?php if(function_exists('pagenavi')) { pagenavi(); } ?>
+        </nav>
     </div>
 </div>
 <?php } else { ?>
 <div class="container post">
     <div class="featurette">
-		<?php  get_template_part('error_snippet');?>
-	</div>
+        <?php  get_template_part('error_snippet');?>
+    </div>
 </div>
 <?php
-
 }
 wp_reset_query();
 get_footer();
