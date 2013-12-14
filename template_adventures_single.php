27f8735a6bddd272f10159e69382726a935e54a5
localhost8080 <localhost8080@gmail.com>
2013-12-14 14:22:21 +0000

    making code easier to read; psr2 is borked for mixed php + html files, unless you echo out allt he html code, which is stupid

--- template_adventures_single.php
+++ template_adventures_single.php
@@ -3,9 +3,7 @@
  * Template Name Posts: Adventures
  */
 get_header();
-?>
-<!-- google_ad_section_start-->
-<?php
+echo '<!-- google_ad_section_start-->';
 if (have_posts()) {
     while (have_posts()) {
         the_post();
@@ -51,10 +49,7 @@ if (have_posts()) {
 <?php
     }
 } else {
-    ?>
-<p>
-		<?php __('Sorry, no posts matched your criteria.','multiloquent');?>
-	</p>
-<?php } ?>
-<!-- google_ad_section_end-->
-<?php get_footer(); ?>
+    echo '<p>' . __('Sorry, no posts matched your criteria.', 'multiloquent') . '</p>';
+}
+echo '<!-- google_ad_section_end-->';
+get_footer();
