a42b62ef2b433a45e7b4260a030334a1dd5dcb12
localhost8080 <localhost8080@gmail.com>
2013-12-14 14:37:42 +0000

    layout issues when rendered

--- metadata.php
+++ metadata.php
@@ -2,11 +2,11 @@
 <li class="pull-right">
 <?php
 echo '<span class="author">' . _e(' by ', 'multiloquent');
-echo '<span><a rel="author" href="' . home_url() . '/about">' . the_author() . '</a>';
-echo '</span></span><span class="divider">/</span><span class="day ' . the_time(__('M', 'multiloquent')) .
-     '"> <span
-        class="fa fa-calendar fa-fw"></span> <time datetime="' .
-     the_time('c') . '">' . the_time(__('M jS, Y', 'multiloquent')) . '</time>';
+echo '<a rel="author" href="' . home_url() . '/about">' . the_author() . '</a>';
+echo '</span>';
+echo '<span class="divider">/</span><span class="day ' . the_time(__('M', 'multiloquent')) . '"> <span
+        class="fa fa-calendar fa-fw"></span> <time datetime="' . the_time('c') . '">' .
+     the_time(__('M jS, Y', 'multiloquent')) . '</time>';
 if (get_the_time('c') != get_the_modified_time('c')) {
     echo '<span class="fa fa-refresh fa-fw"></span> <time datetime="' . the_modified_time('c') . '">' .
          the_modified_time(__('M jS, Y', 'multiloquent')) . '</time>';
