ab4bd3df5f9b14e2b1bc46254a297f97476e4ac1
localhost8080 <localhost8080@gmail.com>
2013-12-14 12:58:15 +0000

    misc changes to header.php

--- header.php
+++ header.php
@@ -3,32 +3,54 @@
 <head>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1 user-scalable=no">
-    <?php 
-    if(preg_match('/^FacebookExternalHit\/.*?/i', $_SERVER['HTTP_USER_AGENT'])){
-    ?>
+    <?php
+    if (preg_match(
+        '/^FacebookExternalHit\/.*?/i', 
+        $_SERVER['HTTP_USER_AGENT'])) {
+        ?>
     <meta name="apple-mobile-web-app-capable" content="yes"><?php } ?>
     <title><?php
-    if (function_exists('ghpseo_output')) {
-        ghpseo_output('main_title');
+    if (function_exists(
+        'ghpseo_output')) {
+        ghpseo_output(
+            'main_title');
     } else {
         if (is_home()) {
-            echo get_bloginfo('name') . ' - ';
-            bloginfo('description');
+            echo get_bloginfo(
+                'name') . ' - ';
+            bloginfo(
+                'description');
         } elseif (is_single() or is_page()) {
-            wp_title('');
-            echo ' - ' . get_bloginfo('name');
+            wp_title(
+                '');
+            echo ' - ' . get_bloginfo(
+                'name');
         } elseif (is_category() || is_tag()) {
-            echo wp_title('');
+            echo wp_title(
+                '');
         } elseif (is_day()) {
-            echo get_bloginfo('name') . ' - ' . get_the_time('d') . '. ' . get_the_time('F') . ' ' . get_the_time('Y');
+            echo get_bloginfo(
+                'name') . ' - ' . get_the_time(
+                'd') . '. ' . get_the_time(
+                'F') . ' ' . get_the_time(
+                'Y');
         } elseif (is_month()) {
-            echo get_bloginfo('name') . ' - ' . get_the_time('F') . ' ' . get_the_time('Y');
+            echo get_bloginfo(
+                'name') . ' - ' . get_the_time(
+                'F') . ' ' . get_the_time(
+                'Y');
         } elseif (is_year()) {
-            echo get_bloginfo('name') . ' - ' . get_the_time('Y');
+            echo get_bloginfo(
+                'name') . ' - ' . get_the_time(
+                'Y');
         } elseif (is_search()) {
-            echo get_bloginfo('name') . ' - &lsaquo;' . esc_html($s, 1) . '&rsaquo;';
+            echo get_bloginfo(
+                'name') . ' - &lsaquo;' . esc_html(
+                $s, 
+                1) . '&rsaquo;';
         } elseif (is_404()) {
-            echo get_bloginfo('name') . '  - 404 - ERROR';
+            echo get_bloginfo(
+                'name') . '  - 404 - ERROR';
         }
     }
     ?></title>
@@ -36,7 +58,8 @@
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
     <?php
     wp_head();
-    if (function_exists('yoast_analytics')) {
+    if (function_exists(
+        'yoast_analytics')) {
         yoast_analytics();
     }
     ?>
@@ -46,4 +69,10 @@
 <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.1/css/font-awesome.css" rel="stylesheet" media="screen"
     type="text/css">
 </head>
-<body <?php if ( is_home() ) { ?> id="home" <?php } body_class();?>>
+<body <?php
+
+if (is_home()) {
+    echo 'id="home"';
+}
+body_class();
+?>>
