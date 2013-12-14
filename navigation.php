0a3f9d24dff21750dd45e94dadcd00f1f7afeeb8
localhost8080 <localhost8080@gmail.com>
2013-12-14 22:35:27 +0000

    fix for phpcs

--- navigation.php
+++ navigation.php
@@ -4,7 +4,7 @@
             <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
             </a>
             <ul class="nav">
-                <li class="active"><a title="<?php bloginfo('name'); ?>" href="<?php echo home_url(); ?>/"><span class="fa fa-desktop fa-fw"></span>
+                <li class="active"><a href="<?php echo home_url(); ?>/"><span class="fa fa-desktop fa-fw"></span>
                         <h5 style="font-size: 17px; display: inline-block; margin: 0 0 0 15px; padding: 0;"><?php bloginfo('name'); ?></h5></a></li>
             </ul>
             <div class="btn-group">
@@ -13,7 +13,7 @@
                 </button>
                 <div class="dropdown-menu" role="menu">
   <?php wp_nav_menu(array('menu' => 'header' )); ?>
-</div>
+                </div>
             </div>
             <ul class="nav nav-collapse collapse search-box">
                 <li><?php get_search_form();?></li>
