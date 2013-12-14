98b1dd6135d62d5946ad87d6fe37f007d547d47b
localhost8080 <localhost8080@gmail.com>
2013-12-14 23:38:30 +0000

    use wordpress built in pagination

--- functions.php
+++ functions.php
@@ -183,16 +183,6 @@ function pagenavi()
     global $wp_query;
     $big = 999999999; // need an unlikely integer
     $args = array(
-        'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-        'format' => '?page=%#%',
-        'total' => $wp_query->max_num_pages,
-        'current' => max( 1, get_query_var( 'paged') ),
-        'show_all' => false,
-        'end_size' => 3,
-        'mid_size' => 2,
-        'prev_next' => True,
-        'prev_text' => __('&laquo; Previous'),
-        'next_text' => __('Next &raquo;'),
         'type' => 'list',
     );
     return '<div class="pagination pagination-centered"><ul>'.paginate_links($args).'</ul></div>';
