af7a49f537ddfa3815bc8620fa9dde29a25f17b6
localhost8080 <localhost8080@gmail.com>
2013-12-14 16:17:27 +0000

    Inline control structures are not allowed

--- breadcrumb.php
+++ breadcrumb.php
@@ -1,6 +1,8 @@
 <nav class="container">
     <ul class="breadcrumb clearfix">
-        <?php if (function_exists('breadcrumbs')) breadcrumbs(); ?>
-        <?php get_template_part('metadata'); ?>
+        <?php if (function_exists('breadcrumbs')){
+            breadcrumbs(); 
+        }
+        get_template_part('metadata'); ?>
     </ul>
 </nav>
