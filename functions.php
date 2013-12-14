f0b8a0e7370d08505024fe0909369140b41f219a
localhost8080 <localhost8080@gmail.com>
2013-12-14 23:31:19 +0000

    removing the huge pagenavi function

--- functions.php
+++ functions.php
@@ -187,141 +187,25 @@ function jb_get_previous_posts_link($label = null)
     }
 }
 
-/**
- * Function that performs a Boxed Style Numbered Pagination (also called Page Navigation).
- * Function is largely based on Version 2.4 of the WP-PageNavi plugin
- */
-function pagenavi($before = '', $after = '')
+
+function pagenavi()
 {
     global $wp_query;
-    $pagenavi_options = array();
-    $pagenavi_options['pages_text'] = ('Page %CURRENT_PAGE% of %TOTAL_PAGES%:');
-    $pagenavi_options['current_text'] = '%PAGE_NUMBER%';
-    $pagenavi_options['page_text'] = '%PAGE_NUMBER%';
-    $pagenavi_options['first_text'] = ('First Page');
-    $pagenavi_options['last_text'] = ('Last Page');
-    $pagenavi_options['next_text'] = 'Next &raquo;';
-    $pagenavi_options['prev_text'] = '&laquo; Previous';
-    $pagenavi_options['dotright_text'] = '...';
-    $pagenavi_options['dotleft_text'] = '...';
-    $pagenavi_options['num_pages'] = 5; // continuous block of page numbers
-    $pagenavi_options['always_show'] = 0;
-    $pagenavi_options['num_larger_page_numbers'] = 0;
-    $pagenavi_options['larger_page_numbers_multiple'] = 5;
-    // If NOT a single Post is being displayed
-    /* http://codex.wordpress.org/Function_Reference/is_single) */
-    if (! is_single()) {
-        $wp_query->request;
-        // intval Get the integer value of a variable
-        /* http://php.net/manual/en/function.intval.php */
-        // $posts_per_page = intval(get_query_var('posts_per_page'));
-        // Retrieve variable in the WP_Query class.
-        /* http://codex.wordpress.org/Function_Reference/get_query_var */
-        $paged = intval(get_query_var('paged'));
-        // $numposts = $wp_query->found_posts;
-        $max_page = $wp_query->max_num_pages;
-        // empty Determine whether a variable is empty
-        /* http://php.net/manual/en/function.empty.php */
-        if (empty($paged) || $paged == 0) {
-            $paged = 1;
-        }
-        $pages_to_show = intval($pagenavi_options['num_pages']);
-        $l_page_to_show = intval($pagenavi_options['num_larger_page_numbers']);
-        $l_page_multiple = intval($pagenavi_options['larger_page_numbers_multiple']);
-        $pages_minus_1 = $pages_to_show - 1;
-        $half_page_start = floor($pages_minus_1 / 2);
-        // ceil Round fractions up (http://us2.php.net/manual/en/function.ceil.php)
-        $half_page_end = ceil($pages_minus_1 / 2);
-        $start_page = $paged - $half_page_start;
-        if ($start_page <= 0) {
-            $start_page = 1;
-        }
-        $end_page = $paged + $half_page_end;
-        if (($end_page - $start_page) != $pages_minus_1) {
-            $end_page = $start_page + $pages_minus_1;
-        }
-        if ($end_page > $max_page) {
-            $start_page = $max_page - $pages_minus_1;
-            $end_page = $max_page;
-        }
-        if ($start_page <= 0) {
-            $start_page = 1;
-        }
-        $l_per_page = $l_page_to_show * $l_page_multiple;
-        // round_num() custom function - Rounds To The Nearest Value.
-        $l_start_page_start = (round_num($start_page, 10) + $l_page_multiple) - $l_per_page;
-        $l_start_page_end = round_num($start_page, 10) + $l_page_multiple;
-        $l_end_page_start = round_num($end_page, 10) + $l_page_multiple;
-        $larger_end_page_end = round_num($end_page, 10) + ($l_per_page);
-        if ($l_start_page_end - $l_page_multiple == $start_page) {
-            $l_start_page_start = $l_start_page_start - $l_page_multiple;
-            $l_start_page_end = $l_start_page_end - $l_page_multiple;
-        }
-        if ($l_start_page_start <= 0) {
-            $l_start_page_start = $l_page_multiple;
-        }
-        if ($l_start_page_end > $max_page) {
-            $l_start_page_end = $max_page;
-        }
-        if ($larger_end_page_end > $max_page) {
-            $larger_end_page_end = $max_page;
-        }
-        if ($max_page > 1 || intval($pagenavi_options['always_show']) == 1) {
-            /* http://php.net/manual/en/function.str-replace.php */
-            /*number_format_i18n(): Converts integer number to format based on locale (wp-includes/functions.php*/
-            $pages_text = str_replace("%CURRENT_PAGE%", number_format_i18n($paged), $pagenavi_options['pages_text']);
-            $pages_text = str_replace("%TOTAL_PAGES%", number_format_i18n($max_page), $pages_text);
-            echo $before . '<div class="pagination pagination-centered"><ul>' . "\n";
-            if (! empty($pages_text)) {
-                echo '<li><span>' . $pages_text . '</span></li>';
-            }
-            // Displays a link to the previous post which exists in chronological order from the current post.
-            /* http://codex.wordpress.org/Function_Reference/previous_post_link */
-            $prev_link = jb_get_previous_posts_link($pagenavi_options['prev_text']);
-            echo '<li>' . $prev_link . '</li>';
-            if ($start_page >= 2 && $pages_to_show < $max_page) {
-                $first_page_text = str_replace("%TOTAL_PAGES%", number_format_i18n($max_page), $pagenavi_options['first_text']);
-                // esc_url(): Encodes < > & " ' (less than, greater than, ampersand, double quote, single quote).
-                /* http://codex.wordpress.org/Data_Validation */
-                // get_pagenum_link():(wp-includes/link-template.php)-Retrieve get links for page numbers.
-                echo '<li><a href="' . untrailingslashit(esc_url(get_pagenum_link())) . '" class="first" title="' . $first_page_text . '">1</a></li>';
-                if (! empty($pagenavi_options['dotleft_text'])) {
-                    echo '<li><span>' . $pagenavi_options['dotleft_text'] . '</span></li>';
-                }
-            }
-            if ($l_page_to_show > 0 && $l_start_page_start > 0 && $l_start_page_end <= $max_page) {
-                for ($i = $l_start_page_start; $i < $l_start_page_end; $i += $l_page_multiple) {
-                    $page_text = str_replace("%PAGE_NUMBER%", number_format_i18n($i), $pagenavi_options['page_text']);
-                    echo '<li><a href="' . untrailingslashit(esc_url(get_pagenum_link($i))) . '" class="single_page" title="' . $page_text . '">' . $page_text . '</a></li>';
-                }
-            }
-            for ($i = $start_page; $i <= $end_page; $i ++) {
-                if ($i == $paged) {
-                    $current_page_text = str_replace("%PAGE_NUMBER%", number_format_i18n($i), $pagenavi_options['current_text']);
-                    echo '<li><span>' . $current_page_text . '</span></li>';
-                } else {
-                    $page_text = str_replace("%PAGE_NUMBER%", number_format_i18n($i), $pagenavi_options['page_text']);
-                    echo '<li><a href="' . untrailingslashit(esc_url(get_pagenum_link($i))) . '" class="single_page" title="' . $page_text . '">' . $page_text . '</a></li>';
-                }
-            }
-            if ($end_page < $max_page) {
-                if (! empty($pagenavi_options['dotright_text'])) {
-                    echo '<li><span>' . $pagenavi_options['dotright_text'] . '</span></li>';
-                }
-                $last_page_text = str_replace("%TOTAL_PAGES%", number_format_i18n($max_page), $pagenavi_options['last_text']);
-                echo '<li><a href="' . untrailingslashit(esc_url(get_pagenum_link($max_page))) . '" class="last" title="' . $last_page_text . '">' . $max_page . '</a></li>';
-            }
-            $next_link = get_next_posts_link($pagenavi_options['next_text'], $max_page);
-            echo '<li>' . $next_link . '</li>';
-            if ($l_page_to_show > 0 && $l_end_page_start < $max_page) {
-                for ($i = $l_end_page_start; $i <= $larger_end_page_end; $i += $l_page_multiple) {
-                    $page_text = str_replace("%PAGE_NUMBER%", number_format_i18n($i), $pagenavi_options['page_text']);
-                    echo '<li><a href="' . untrailingslashit(esc_url(get_pagenum_link($i))) . '" class="single_page" title="' . $page_text . '">' . $page_text . '</a></li>';
-                }
-            }
-            echo '</ul></div>' . $after . "\n";
-        }
-    }
+    $big = 999999999; // need an unlikely integer
+    $args = array(
+        'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+        'format' => '?page=%#%',
+        'total' => $wp_query->max_num_pages,
+        'current' => max( 1, get_query_var( 'paged') ),
+        'show_all' => false,
+        'end_size' => 3,
+        'mid_size' => 2,
+        'prev_next' => True,
+        'prev_text' => __('&laquo; Previous'),
+        'next_text' => __('Next &raquo;'),
+        'type' => 'list',
+    );
+    return '<div class="pagination pagination-centered"><ul>'.paginate_links($args).'</ul></div>';
 }
 
 function get_random_solid_class($class = '')
