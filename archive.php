db431541ea7a92e1e339abeb23f8904869ff3a80
localhost8080 <localhost8080@gmail.com>
2013-12-15 00:31:34 +0000

    pagination

--- archive.php
+++ archive.php
@@ -85,22 +85,17 @@ if (have_posts()) {
         </section>
         <nav class="navitems article white2">
             <?php
-            
-
-
-$total_pages = $wp_query->max_num_pages;
-
-if ($total_pages > 1){
-
-  $current_page = max(1, get_query_var('paged'));
-  
-  echo paginate_links(array(
-      'base' => get_pagenum_link(1) . '%_%',
-      'format' => '/page/%#%',
-      'current' => $current_page,
-      'total' => $total_pages,
-    ));
-}
+    $total_pages = $wp_query->max_num_pages;
+    if ($total_pages > 1) {
+        $current_page = max(1, get_query_var('paged'));
+        echo paginate_links(array(
+            'base' => get_pagenum_link(1) . '%_%',
+            'format' => '/page/%#%',
+            'current' => $current_page,
+            'total' => $total_pages,
+            'type' => 'list'
+        ));
+    }
     ?>
         </nav>
     </div>
