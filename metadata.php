00c0e0cb0390a7b4c0e2350da58d1a39ab645aee
localhost8080 <localhost8080@gmail.com>
2013-12-17 21:37:54 +0000

    __ instead of _e

--- metadata.php
+++ metadata.php
@@ -5,4 +5,4 @@ if (get_the_time('c') != get_the_modified_time('c')) {
     echo '<span class="fa fa-refresh fa-fw"></span> <time datetime="' . get_the_modified_time('c') . '">' . get_the_modified_time(__('M jS, Y', 'multiloquent')) . '</time>';
 }
 echo '</li>';
-echo '<li class="pull-right author">' . _e(' by ', 'multiloquent') . '<a rel="author" href="' . home_url() . '/about">' . get_the_author() . '</a></li>';
+echo '<li class="pull-right author">' . __(' by ', 'multiloquent') . '<a rel="author" href="' . home_url() . '/about">' . get_the_author() . '</a></li>';
