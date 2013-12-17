bc54eabb293fc32ce71b4d6d625c1cb16be334cb
localhost8080 <localhost8080@gmail.com>
2013-12-17 21:48:43 +0000

    changing code for old version

--- metadata.php
+++ metadata.php
@@ -1,8 +1,14 @@
+<li class="pull-right">
 <?php
-echo '<li class="pull-right">' . get_edit_post_link() . '</li>';
-echo '<li class="day pull-right ' . get_the_time(__('M', 'multiloquent')) . '"><span class="fa fa-calendar fa-fw"></span> <time datetime="' . get_the_time('c') . '">' . get_the_time(__('M jS, Y', 'multiloquent')) . '</time>';
+echo '<span class="author">' . _e(' by ', 'multiloquent');
+echo '<a rel="author" href="' . home_url() . '/about">' . get_the_author() . '</a>';
+echo '</span>';
+echo '<span class="day ' . get_the_time(__('M', 'multiloquent')) . '"> <span
+        class="fa fa-calendar fa-fw"></span> <time datetime="' . get_the_time('c') . '">' . get_the_time(__('M jS, Y', 'multiloquent')) . '</time>';
 if (get_the_time('c') != get_the_modified_time('c')) {
     echo '<span class="fa fa-refresh fa-fw"></span> <time datetime="' . get_the_modified_time('c') . '">' . get_the_modified_time(__('M jS, Y', 'multiloquent')) . '</time>';
 }
-echo '</li>';
-echo '<li class="pull-right author">' . __(' by ', 'multiloquent') . '<a rel="author" href="' . home_url() . '/about">' . get_the_author() . '</a></li>';
+echo '</span>';
+edit_post_link();
+?>
+</li>
