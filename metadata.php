93667bb0340a280d73826fbd08dd2289ef97fd1a
localhost8080 <localhost8080@gmail.com>
2013-12-17 21:27:02 +0000

    ...

--- metadata.php
+++ metadata.php
@@ -1,5 +1,5 @@
 <?php
-echo '<li class="pull-right">' . edit_post_link() . '</li>';
+echo '<li class="pull-right">' . get_edit_post_link() . '</li>';
 echo '<li class="day pull-right ' . get_the_time(__('M', 'multiloquent')) . '"><span class="fa fa-calendar fa-fw"></span> <time datetime="' . get_the_time('c') . '">' . get_the_time(__('M jS, Y', 'multiloquent')) . '</time>';
 if (get_the_time('c') != get_the_modified_time('c')) {
     echo '<span class="fa fa-refresh fa-fw"></span> <time datetime="' . get_the_modified_time('c') . '">' . get_the_modified_time(__('M jS, Y', 'multiloquent')) . '</time>';
