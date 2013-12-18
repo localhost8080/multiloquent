964f229dae72e991a0309931e6ef98fa80e50d10
localhost8080 <localhost8080@gmail.com>
2013-12-18 23:34:28 +0000

    Signed-off-by: localhost8080 <localhost8080@gmail.com>

--- navigation.php
+++ navigation.php
@@ -8,8 +8,8 @@
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="/category"><span class="fa fa-folder-o fa-fw"></span>categories</a></li>
-                    <?php /*<li><a href="/tags"><span class="fa fa-folder-o fa-fw"></span>tags</a></li>    */?>
+                <?php /*<li><a href="/category"><span class="fa fa-folder-o fa-fw"></span>categories</a></li>
+                    <li><a href="/tags"><span class="fa fa-folder-o fa-fw"></span>tags</a></li>    */?>
                     <li><?php get_search_form();?></li>
             </ul>
         </div>
