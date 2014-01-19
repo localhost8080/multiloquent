9075df38e781d45bc733ce4e2db799bdd10fd945
localhost8080 <localhost8080@gmail.com>
2014-01-19 16:16:44 +0000

    top nav menu as bootstrap dropdown

--- navigation.php
+++ navigation.php
@@ -10,7 +10,21 @@
             <ul class="nav navbar-nav navbar-right">
                 <li><?php get_search_form();?></li>
             </ul>
-            <?php wp_nav_menu( array( 'theme_location' => 'Header Navigation' ) );
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'Header Navigation',
+                'container' => '',
+                'container_class' => '',
+                'container_id' => '',
+                'menu_class' => 'dropdown-menu',
+                'menu_id' => '',
+                'before' => '<li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>',
+                'after' => '</li>',
+                'link_before' => '',
+                'link_after' => '',
+                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>'
+            ));
             if (is_active_sidebar(1)) {
                 dynamic_sidebar(1);
             }
