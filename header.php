3b0ea572d5ee8d7b47637c9e92d0f2fcfbb0dfec
localhost8080 <localhost8080@gmail.com>
2014-01-19 14:39:03 +0000

    All stylesheets (other than style.css) should be enqueued via function hooked into wp_enqueue_script from functions.php.
    Do not load scripts and stylesheets from CDN. To use those, you need to
    package them with the theme.
    
    Remove hard coded scripts and stylesheets from header.php.

--- header.php
+++ header.php
@@ -12,19 +12,12 @@ if (preg_match('/^FacebookExternalHit\/.*?/i', $_SERVER['HTTP_USER_AGENT'])) {
     ?></title>
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
     <?php
-    
     if (function_exists('yoast_analytics')) {
         yoast_analytics();
     }
+    echo '<link type="text/css" rel="stylesheet" media="screen" href="' . get_template_directory_uri() . '/style.css?v=' . multiloquent_version() . '">';
+    wp_head();
     ?>
-<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-<link type="text/css" rel="stylesheet" media="screen" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
-<link type="text/css" rel="stylesheet" media="screen" href="//netdna.bootstrapcdn.com/font-awesome/4.0.1/css/font-awesome.min.css">
-<?php
-echo '<link type="text/css" rel="stylesheet" media="screen" href="' . get_template_directory_uri() . '/style.css?v=' . multiloquent_version() . '">';
-echo '<link type="text/css" rel="stylesheet" media="print" href="' . get_template_directory_uri() . '/print.css">';
-wp_head();
-?>
 </head>
 <body <?php
 if (is_home()) {
