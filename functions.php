adacdabacef74ec7a5de85007306126613b50de7
localhost8080 <localhost8080@gmail.com>
2014-01-21 21:39:36 +0000

    changing file names

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
 <?php
-get_template_part('function_definitions');
+get_template_part('function-definitions');
 add_action('init', 'multiloquent_init');
 add_action('after_setup_theme', 'multiloquent_register');
