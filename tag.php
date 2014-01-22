dfe278c4aaeb7e199789e3c89adafa984a05f8be
localhost8080 <localhost8080@gmail.com>
2014-01-22 18:29:52 +0000

    category and tag specific archive pages

---
+++ tag.php
@@ -0,0 +1,44 @@
+<?php
+get_header();
+if (have_posts()) {
+    ?>
+<div class="jumbotron">
+    <div class="container">
+        <header>
+            <h1 class="article_title">
+			<?php
+    _e('Posts Tagged', 'multiloquent');
+    echo ' &#8216;';
+    single_tag_title();
+    echo '&#8217;';
+    ?>
+            </h1>
+        </header>
+    </div>
+</div>
+<?php  	$post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
+<div class="container post">
+    <div class="featurette">
+        <section class="row">
+				<?php multiloquent_render_the_archive();?>
+		</section>
+        <section>
+			<?php get_template_part('advert');?>
+		</section>
+        <nav class="navitems article white2">
+            <div class="pagination pagination-centered">
+				<?php multiloquent_render_pagingation(); ?>
+			</div>
+        </nav>
+    </div>
+</div>
+<?php } else { ?>
+<div class="container post">
+    <div class="featurette">
+		<?php  get_template_part('error_snippet');?>
+	</div>
+</div>
+<?php
+}
+wp_reset_query();
+get_footer();
