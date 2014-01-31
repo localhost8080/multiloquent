ae5dbdf2cb09cd12a081265713c459698f969519
localhost8080 <localhost8080@gmail.com>
2014-01-31 20:55:00 +0000

    remove the &raquo from the wp_title

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1 user-scalable=no">
-<title><?php wp_title(); ?></title>
+<title><?php wp_title(''); ?></title>
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
     <?php
     if (function_exists('yoast_analytics')) {
