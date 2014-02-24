d22dc69c60f4522a19906fc9c40771e165ca71af
development <development@jonathansblog.co.uk>
2014-02-24 22:41:58 +0000

    fixing tab indents

--- function-definitions.php
+++ function-definitions.php
@@ -204,20 +204,20 @@ function multiloquent_render_pagingation()
     $total_pages = $wp_query->max_num_pages;
     // check if search result
     if(is_search()){
-    	$format = '?paged=%#%';
+        $format = '?paged=%#%';
     } else {
         $format = '/page/%#%';
     }
-        if ($total_pages > 1) {
-            $current_page = max(1, get_query_var('paged'));
-            echo paginate_links(array(
-                'base' => get_pagenum_link(1) . '%_%',
-                'format' => $format,
-                'current' => $current_page,
-                'total' => $total_pages,
-                'type' => 'list'
-            ));
-        }
+    if ($total_pages > 1) {
+        $current_page = max(1, get_query_var('paged'));
+        echo paginate_links(array(
+            'base' => get_pagenum_link(1) . '%_%',
+            'format' => $format,
+            'current' => $current_page,
+            'total' => $total_pages,
+            'type' => 'list'
+        ));
+    }
     
 }
 
