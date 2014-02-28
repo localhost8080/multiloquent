e3d637bf6adc579dc1e9cacea4bb8f801835bcd7
development <development@jonathansblog.co.uk>
2014-02-28 21:36:27 +0000

    check to see if the author has a url set - if they do, use it as the rel=author url

--- metadata.php
+++ metadata.php
@@ -1,7 +1,15 @@
 <li class="pull-right">
 <?php
 echo '<span class="author">' . _e(' by ', 'multiloquent');
-the_author_posts_link();
+
+// check to see if the user has a url set in their meta data; if they have then use it as the rel=author link
+$user_url = get_the_author_meta('user_url');
+if(!empty($user_url)){
+    echo '<a href='.$user_url.'" rel="author">by '.the_author().'</a>';
+} else {
+    the_author_posts_link();
+}
+
 echo '</span>';
 echo '<span class="day ' . get_the_time(__('M', 'multiloquent')) . '"> <span
         class="fa fa-calendar fa-fw"></span> <time datetime="' . get_the_time('c') . '">' . get_the_time(__('M jS, Y', 'multiloquent')) . '</time>';
