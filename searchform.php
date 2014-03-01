7c0b38446045f4099bba861bc956e37200128083
development <development@jonathansblog.co.uk>
2014-03-01 14:17:42 +0000

    oops

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form action="<?php echo esc_url(home_url('/')); ?>" id="search_form" class="col-sm-12 col-md-12 col-lg-12">
     <div class="input-prepend">
-        <label title="search" for="s" class="add-on"><span class="fa fa-search fafw"></span></label> <input type="text" id="s" name="s" autocomplete="off" placeholder="Username"> <input type="hidden" value="the_search_text" name="action">
+        <label title="search" for="s" class="add-on"><span class="fa fa-search fafw"></span></label> <input type="text" id="s" name="s" autocomplete="off" placeholder="Search"> <input type="hidden" value="the_search_text" name="action">
     </div>
 </form>
