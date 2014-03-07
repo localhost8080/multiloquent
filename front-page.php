84d830fb4c2c07025c2fbe63ce62b4eea29d2736
development <development@jonathansblog.co.uk>
2014-03-07 18:07:36 +0000

    option for front page to use pagination of blog posts

--- front-page.php
+++ front-page.php
@@ -1,22 +1,25 @@
 <?php
 get_header();
 echo multiloquent_paralax_slider();
-?>
-
+if ('posts' == get_option('show_on_front')) {
+    include (get_home_template());
+} else {
+    ?>
 <!-- google_ad_section_start-->
 <div class="jumbotron">
     <div class="container">
-    <?php
+            <?php
     while (have_posts()) {
         the_post();
         the_content("<p class=\"serif\">" . __('Read the rest of this page', 'multiloquent') . " &raquo;</p>");
     }
     ?>    
-    </div>
+            </div>
 </div>
 <!-- google_ad_section_end-->
 <div class="container">
-    <?php get_template_part('advert');?>
-</div>
-<?php
+            <?php get_template_part('advert');?>
+        </div>
+<?
+}
 get_footer();
