b07f489b759e16b4823e0f819eb1ad2a969fb001
development <development@jonathansblog.co.uk>
2014-03-10 22:14:30 +0000

    fix

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
 <?php
-multiloquent_get_template_part('function-definitions');
+require_once (trailingslashit(get_template_directory()) . 'function-definitions.php');
 add_action('after_setup_theme', 'multiloquent_register');
 add_action('wp_head', 'multiloquent_customize_css');
