a54e8b2ea96014aacb8c2b7c88e53b3ceb2330b0
development <development@jonathansblog.co.uk>
2014-03-16 20:30:52 +0000

    sidebar mods

--- footer.php
+++ footer.php
@@ -1,8 +1,35 @@
 <?php multiloquent_get_template_part('navigation'); ?>
+<?php get_sidebar(); ?>
 <footer class="well">
     <div class="container">
         <aside>
-            <?php get_sidebar(); ?>
+            
+<div class="row">
+    <div class="col-sm-6 col-md-6 col-lg-6 no-bullets">
+        <p class="nav-header">Useful Stuff</p>
+                <?php
+                // sidebar 10 for some things in footer
+                if (is_active_sidebar(9)) {
+                    dynamic_sidebar(9);
+                }
+                ?>
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-6 no-bullets">
+        <p class="nav-header">More Useful Stuff</p>
+                <?php
+                // sidebar 10 for some things in footer
+                if (is_active_sidebar(10)) {
+                    dynamic_sidebar(10);
+                }
+                ?>
+    </div>
+    <p>
+        <span class="fa fa-gamepad fa-fw"></span><span style="font-size: 12px;">This website is most definitely not best viewed in Netscape</span>
+    </p>
+    <p>
+        <span class="fa fa-folder-open-o fa-fw"></span> multiloquent html5 theme <?php echo multiloquent_version();?>
+    </p>
+</div>
         </aside>
     </div>
 </footer>
