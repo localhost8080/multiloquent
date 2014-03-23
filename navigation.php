4324365872d63583cb13d654f9d849edb546baa0
development <development@jonathansblog.co.uk>
2014-03-23 21:32:43 +0000

    removing icon

--- navigation.php
+++ navigation.php
@@ -2,10 +2,7 @@
 <a href="javascript:void(0);" class="navbar-brand sidebar-toggle"><span class="fa fa-bars"></span></a>
     <div class="container">
         <div class="navbar-header">
-
-            <?php echo '<a class="navbar-brand" title="'. get_bloginfo('name').'" href="'. esc_url(home_url('/')) .'"><span class="fa fa-desktop fa-fw"></span>'. get_bloginfo('name').'</a>';?>
-        
+            <?php echo '<a class="navbar-brand" title="'. get_bloginfo('name').'" href="'. esc_url(home_url('/')) .'">'. get_bloginfo('name').'</a>';?>       
         </div>
-
     </div>
 </nav>
