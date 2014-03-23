ed4082b84787c84a6a22771ea3616356aa076c13
development <development@jonathansblog.co.uk>
2014-03-23 21:37:36 +0000

    fixes

--- navigation.php
+++ navigation.php
@@ -1,6 +1,6 @@
 <nav role="navigation" class="navbar navbar-default navbar-fixed-top">
 <a href="javascript:void(0);" class="navbar-brand sidebar-toggle"><span class="fa fa-bars"></span></a>
-    <div class="container">
+    <div class="container sitename">
         <div class="navbar-header">
             <?php echo '<a class="navbar-brand" title="'. get_bloginfo('name').'" href="'. esc_url(home_url('/')) .'">'. get_bloginfo('name').'</a>';?>       
         </div>
