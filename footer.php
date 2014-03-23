1c1971c2957743c7aba468bd600256750364d4b7
development <development@jonathansblog.co.uk>
2014-03-23 20:34:10 +0000

    removing

--- footer.php
+++ footer.php
@@ -74,9 +74,7 @@ jQuery('.sidebar-toggle').click(function(){
     }
 });
 
-jQuery('.sidebar .menu ul > li.page_item_has_children').click(function(){
-    jQuery(this).children('ul.children').slideToggle();
-});
+
 
 
 
