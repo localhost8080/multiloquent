b3fc37e142adc8df87a12dec2e75bd2082716286
development <development@jonathansblog.co.uk>
2014-03-23 21:26:53 +0000

    cleanup

--- sidebar.php
+++ sidebar.php
@@ -2,20 +2,19 @@
 
             <?php
             get_search_form();
-            wp_nav_menu(array('theme_location' => 'primary-menu'));
+            wp_nav_menu(array(
+                'theme_location' => 'primary-menu'
+            ));
             if (is_active_sidebar(1)) {
                 dynamic_sidebar(1);
             }
+            /* recent posts. */
+            if (is_active_sidebar(4)) {
+                dynamic_sidebar(4);
+            }
+            if (is_active_sidebar(5)) {
+                dynamic_sidebar(5);
+            }
             ?>
- 
-        <?php
-        /* recent posts. */
-        if (is_active_sidebar(4)) {
-            dynamic_sidebar(4);
-        }
-        if (is_active_sidebar(5)) {
-            dynamic_sidebar(5);
-        }
-        ?>
 
 </div>
