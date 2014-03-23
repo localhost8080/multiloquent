f5b08cf07bcc36577fc565ae3742b15c61d3e9d0
development <development@jonathansblog.co.uk>
2014-03-23 21:56:08 +0000

    title text

--- navigation.php
+++ navigation.php
@@ -1,7 +1,6 @@
 <nav role="navigation" class="navbar navbar-default navbar-fixed-top">
-    
-    <a href="javascript:void(0);" class="navbar-brand sidebar-toggle"><span class="fa fa-bars"></span></a>
     <span class="navbar-header">
-    <?php echo '<a class="navbar-brand" title="'. get_bloginfo('name').'" href="'. esc_url(home_url('/')) .'">'. get_bloginfo('name').'</a>';?>       
+    <a href="javascript:void(0);" class="navbar-brand sidebar-toggle"><span class="fa fa-bars"></span></a>  
+    <?php echo '<a class="navbar-brand title_text" title="'. get_bloginfo('name').'" href="'. esc_url(home_url('/')) .'">'. get_bloginfo('name').'</a>';?>       
     </span>
 </nav>
