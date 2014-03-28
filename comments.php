7d134fbcf9ca56bf6055c1706c17c0c2f778c537
development <development@jonathansblog.co.uk>
2014-03-28 21:08:01 +0000

    commetns

--- comments.php
+++ comments.php
@@ -11,7 +11,9 @@ if (have_comments()) {
     <div>
         <h3><?php printf(_n('One Response to &ldquo;%2$s&rdquo;', '%1$s Responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'multiloquent'), number_format_i18n(get_comments_number()), multiloquent_post_title()); ?></h3>
         <ol class="media-list">
-          <?php wp_list_comments(); ?>
+           
+          <?php wp_list_comments(array('style' => 'ol','format' => 'html5')); ?>
+          
         </ol>
     
         <?php if (get_comment_pages_count() > 1 && get_option('page_comments')) { ?>
