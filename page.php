b9dcbfb82f91d65391ab4b687dbb763cde53238d
development <development@jonathansblog.co.uk>
2014-03-29 11:49:09 +0000

    replacing the page with the content from the full width post...

--- page.php
+++ page.php
@@ -1,14 +1,12 @@
 <?php
+/*
+ * Template Name Posts: Full width
+ */
 get_header();
-?>
-<div class="container">
-    <!-- google_ad_section_start-->
-    <article>
-    <?php
-    rewind_posts();
+echo '<!-- google_ad_section_start-->';
+if (have_posts()) {
     while (have_posts()) {
         the_post();
-        
         multiloquent_get_template_part('featuredposts');
         multiloquent_get_template_part('featuredimage');
         echo '<div id="post-' . get_the_ID() . '" ';
@@ -16,31 +14,69 @@ get_header();
         echo '>';
         if (empty($multiloquent_hide_h1_tag)) {
             ?>
-        <h1 class="container featurette-heading">
-                        <?php echo multiloquent_post_title();?>
-                    </h1>
-        <?php
-                }
-        
-        
-        
-        the_content("<p class=\"serif\">" . 'Read the rest of this page' . " &raquo;</p>");
-    }
-    ?>
-    </article>
-     <?php
-    if (comments_open()) {
+<h1 class="featurette-heading container">
+<?php echo multiloquent_post_title();?>
+</h1>
+<?php
+        }
+        multiloquent_get_template_part('breadcrumb');
         ?>
+<div class="container">
+        <?php
+        if ($values = get_post_custom_values("leadvideo")) {
+            echo '<iframe width="100%" height="400" src="//www.youtube.com/embed/';
+            echo $values[0];
+            echo '" frameborder="0" allowfullscreen></iframe>';
+        }
+        remove_filter('the_content', 'sharing_display', 19);
+        remove_filter('the_excerpt', 'sharing_display', 19);
+        the_content("<p class=\"serif\">" . 'Read the rest of this page' . " &raquo;</p>");
+        wp_link_pages("<p><strong>Pages:</strong>", '</p>', 'number');
+        ?>
+        </div>
+<?php
+        multiloquent_get_template_part('advert');
+        multiloquent_get_template_part('social');
+        if (comments_open()) {
+            ?>
 <section class="comments_full container">
-        <h3 class="hidden-lg">Comments for <?php echo multiloquent_post_title() ?></h3>
+    <h3 class="hidden-lg">Comments for <?php echo multiloquent_post_title(); ?></h3>
                     <?php comments_template(); ?>    
                 </section>
 <?php
-    }
-    ?>
-    <!-- google_ad_section_end-->
-    
-            <?php multiloquent_get_template_part('advert');?>
+        }
+        multiloquent_get_template_part('advert');
+        ?>
+<section class="container">
+    <div class="tagcloud">
+        <div class="tag-cloud">
+            <h3>Tags for <?php echo multiloquent_post_title(); ?></h3>
+            <?php
+        $posttags = get_the_tags();
+        if ($posttags) {
+            foreach ($posttags as $tag) {
+                // if($tag->count > 5){
+                echo '<a class="label ';
+                echo multiloquent_get_random_solid_class($tag->slug);
+                echo '" rel="nofollow" href="' . get_tag_link($tag->term_id) . '"><span class="icon-tag icon-white"></span> ' . $tag->name . '</a>';
+                // }
+            }
+        }
+        ?>
         </div>
+    </div>
+</section>
 <?php
+        if (function_exists('related_posts')) {
+            related_posts();
+        }
+        next_post_link('%link', '<span class="next_link btn btn-default btn-lg"><span class="fa fa-chevron-left"></span></span>', TRUE);
+        previous_post_link('%link', '<span class="prev_link btn btn-default btn-lg"><span class="fa fa-chevron-right"></span></span>', TRUE);
+        multiloquent_get_template_part('advert');
+        echo '</div>';
+    }
+} else {
+    echo '<p>Sorry, no posts matched your criteria.<p>';
+}
+echo '<!-- google_ad_section_end-->';
 get_footer();
