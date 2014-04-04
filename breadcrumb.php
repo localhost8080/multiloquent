d2165256e5338b58b14a30fef8bdbb644d5036c3
development <development@jonathansblog.co.uk>
2014-04-04 20:42:09 +0100

    test

--- breadcrumb.php
+++ breadcrumb.php
@@ -1,7 +1,7 @@
 <nav class="container hidden-xs">
     <ul class="breadcrumb clearfix">
 		<?php
-//echo multiloquent_breadcrumbs();
+echo multiloquent_breadcrumbs();
 //get_template_part('metadata');
 ?>
 	</ul>
