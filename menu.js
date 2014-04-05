533993bae49f47d35aabfcfdd6a631f8c387a134
development <development@jonathansblog.co.uk>
2014-04-05 12:57:25 +0100

    $ instead of jQuery

--- menu.js
+++ menu.js
@@ -1,43 +1,43 @@
 function resize_sidebar() {
-    if (jQuery('.wrapper').height() < jQuery('.sidebar').height()) {
+    if ($('.wrapper').height() < $('.sidebar').height()) {
         // the sidebar is bigger than the wrapper
-        jQuery('.wrapper').css("height", jQuery('.sidebar').height());
+        $('.wrapper').css("height", $('.sidebar').height());
     }
 }
 // resize the sidebar onload
-jQuery(document).load(function() {
+$(document).load(function() {
     resize_sidebar();
 });
 // need to do it on ready for pages, for some reason it doesnt fire in webkit
-jQuery(document).ready(function() {
+$(document).ready(function() {
     resize_sidebar();
 });
-jQuery('.sidebar-toggle').click(function() {
+$('.sidebar-toggle').click(function() {
     // total fail fallback - if for some reason the page hasnt resized, force it
     // to resize now
     resize_sidebar();
-    if (jQuery('.sidebar').css("margin-left") != '0px') {
+    if ($('.sidebar').css("margin-left") != '0px') {
         // its not visible
-        jQuery('.prev_link').hide();
-        jQuery('.next_link').hide();
-        jQuery('.wrapper').css("overflow", "hidden");
-        jQuery('.wrapper').css("margin-left", "300px");
-        jQuery('.sidebar').css("margin-left", "0px");
+        $('.prev_link').hide();
+        $('.next_link').hide();
+        $('.wrapper').css("overflow", "hidden");
+        $('.wrapper').css("margin-left", "300px");
+        $('.sidebar').css("margin-left", "0px");
     } else {
         // its already on the screen
-        jQuery('.wrapper').css("margin-left", "0px");
-        jQuery('.sidebar').css("margin-left", "-300px");
-        jQuery('.prev_link').show();
-        jQuery('.next_link').show();
+        $('.wrapper').css("margin-left", "0px");
+        $('.sidebar').css("margin-left", "-300px");
+        $('.prev_link').show();
+        $('.next_link').show();
     }
 });
 
-jQuery('.sidebar .menu a').click(function(event) {
+$('.sidebar .menu a').click(function(event) {
     // stop the link click targetting the containing element
     event.stopPropagation();
 });
-jQuery('.sidebar .menu ul > li.page_item_has_children').click(function(event) {
+$('.sidebar .menu ul > li.page_item_has_children').click(function(event) {
     // this breaks the links though...
-    jQuery(this).children('ul.children').slideToggle('400', resize_sidebar);
+    $(this).children('ul.children').slideToggle('400', resize_sidebar);
     event.stopPropagation();
 });
