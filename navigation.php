7d5cf41e340c4d130adb0c59c956e775f40e2b1b
development <development@jonathansblog.co.uk>
2014-04-05 22:33:21 +0100

    adding a navigation title to the menu anchor tag

--- navigation.php
+++ navigation.php
@@ -1,6 +1,6 @@
 <nav role="navigation" class="navbar navbar-default navbar-fixed-top">
     <span class="navbar-header">
-    <a href="javascript:void(0);" class="navbar-brand sidebar-toggle"><span class="fa fa-bars"></span></a>  
+    <a title="navigation menu" href="javascript:void(0);" class="navbar-brand sidebar-toggle"><span class="fa fa-bars"></span></a>  
     <?php echo '<a class="navbar-brand title_text" title="'. get_bloginfo('name').'" href="'. esc_url(home_url('/')) .'">'. get_bloginfo('name').'</a>';?>       
     </span>
 </nav>
