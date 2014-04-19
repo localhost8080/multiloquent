17ef7fcbb1c8f935f86d6545151ca0f585bfaeb1
development <development@jonathansblog.co.uk>
2014-04-19 19:36:43 +0100

    global variable

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+global $hide_the_footer_links;
 require_once (trailingslashit(get_template_directory()) . 'function-definitions.php');
 add_action('after_setup_theme', 'multiloquent_register');
 add_action('wp_head', 'multiloquent_customize_css');
