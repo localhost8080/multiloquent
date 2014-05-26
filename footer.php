c59c3829e4a3eceee6bcd5d70a359ccfc3e16672
development <development@jonathansblog.co.uk>
2014-05-26 11:37:11 +0100

    code formatting

--- footer.php
+++ footer.php
@@ -1,10 +1,9 @@
 </div>
 <?php
 get_sidebar();
-
 get_template_part('navigation');
-if(empty($hide_the_footer_links)){
-?>
+if (empty($hide_the_footer_links)) {
+    ?>
 <footer class="well">
     <div class="container">
         <aside>
@@ -12,38 +11,38 @@ if(empty($hide_the_footer_links)){
                 <div class="col-sm-6 col-md-6 col-lg-6 no-bullets mb">
                     
                             <?php
-                            if (is_active_sidebar(6)) {
-                                dynamic_sidebar(6);
-                            }
-                            ?>
+    if (is_active_sidebar(6)) {
+        dynamic_sidebar(6);
+    }
+    ?>
                 </div>
                 <div class="col-sm-6 col-md-6 col-lg-6 no-bullets mb">
                  
                             <?php
-                            if (is_active_sidebar(7)) {
-                                dynamic_sidebar(7);
-                            }
-                            ?>
+    if (is_active_sidebar(7)) {
+        dynamic_sidebar(7);
+    }
+    ?>
                 </div>
             </div>
             <div class="row">
                 <div class="col-sm-6 col-md-6 col-lg-6 no-bullets">
                     <p class="nav-header">Useful Stuff</p>
                             <?php
-                            // sidebar 10 for some things in footer
-                            if (is_active_sidebar(9)) {
-                                dynamic_sidebar(9);
-                            }
-                            ?>
+    // sidebar 10 for some things in footer
+    if (is_active_sidebar(9)) {
+        dynamic_sidebar(9);
+    }
+    ?>
                 </div>
                 <div class="col-sm-6 col-md-6 col-lg-6 no-bullets">
                     <p class="nav-header">More Useful Stuff</p>
                             <?php
-                            // sidebar 10 for some things in footer
-                            if (is_active_sidebar(10)) {
-                                dynamic_sidebar(10);
-                            }
-                            ?>
+    // sidebar 10 for some things in footer
+    if (is_active_sidebar(10)) {
+        dynamic_sidebar(10);
+    }
+    ?>
                 </div>
             </div>
         </aside>
