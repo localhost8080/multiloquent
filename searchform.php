7602671af4a6f8d9051c8eac486da795d63e38b1
development <development@jonathansblog.co.uk>
2014-06-22 17:21:40 +0100

    search label

--- searchform.php
+++ searchform.php
@@ -9,10 +9,11 @@
 ?>
 <form action="<?php echo esc_url(home_url('/')); ?>" id="search_form" class="mb">
     <div class="input-group">
+    <span class="input-group-addon">
+            <label title="search" for="s" class="" style="margin:0"><span class="fa fa-search fafw"></span></label>
+        </span>
         <input type="text" id="s" name="s" autocomplete="off" placeholder="Search" class="form-control"> 
         <input type="hidden" value="the_search_text" name="action">
-        <span class="input-group-btn">
-            <label title="search" for="s" class="btn btn-default" style="margin:0">Go!</label>
-        </span>
+        
     </div>   
 </form>
