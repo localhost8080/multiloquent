dacb0481088e42e55fe1c0d42d15dbff60c04b43
development <development@jonathansblog.co.uk>
2014-06-22 17:13:24 +0100

    nicer label and button

--- searchform.php
+++ searchform.php
@@ -8,9 +8,11 @@
 
 ?>
 <form action="<?php echo esc_url(home_url('/')); ?>" id="search_form" class="mb">
-    <div class="input-prepend">
-        <label title="search" for="s" class="add-on"></label>
+    <div class="input-group">
         <input type="text" id="s" name="s" autocomplete="off" placeholder="Search" class="form-control"> 
         <input type="hidden" value="the_search_text" name="action">
-    </div>
+        <span class="input-group-btn">
+            <span class="btn btn-default"><label title="search" for="s" class="add-on">Go!</label></span>
+        </span>
+    </div>   
 </form>
