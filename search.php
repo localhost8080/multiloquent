6814e03b0cccc19b8a92386287f5e9505c4bdbcd
jonathan <jonathan@adeogroup.co.uk>
2014-09-13 16:25:23 +0100

    Csrf nonce

--- search.php
+++ search.php
@@ -5,7 +5,7 @@
  * @package multiloquent\template_parts
  */
 
-if (empty( $_REQUEST['name_of_nonce_field'])  || ! wp_verify_nonce( $_REQUEST['search'], 'search' ) ) {
+if (empty( $_REQUEST['search'])  || ! wp_verify_nonce( $_REQUEST['search'], 'search' ) ) {
     // 404 ?
     get_header();
     echo '<div class="container post"> ';
