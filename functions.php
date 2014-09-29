753fc03efd576aa2ab66c09507b9f3b2ef69422b
jonathan <jonathan@adeogroup.co.uk>
2014-09-29 13:34:06 +0100

    Oo mods

--- functions.php
+++ functions.php
@@ -11,5 +11,4 @@ require_once (trailingslashit(get_template_directory()) . 'multiloquent-base.php
 global $multiloquent;
 $multiloquent = new multiloquent_base();
 
-add_action('after_setup_theme', 'multiloquent_register($multiloquent)');
-add_action('wp_head', $multiloquent->multiloquent_customize_css());
+
