77937833c24a2a1b4a4b095eee2522e199629770
jonathan <jonathan@adeogroup.co.uk>
2018-03-30 19:54:58 +0100

    navbar fixes

--- navigation.php
+++ navigation.php
@@ -10,11 +10,11 @@
  */
 
 ?>
-<nav role="navigation" class="navbar navbar-default navbar-fixed-top">
-    <div class="navbar-header">
+<nav role="navigation" class="navbar fixed-top navbar-light bg-light">
+    <div class="">
         <a title="navigation menu" href="javascript:void(0);" class="navbar-brand sidebar-toggle">
             <span class="fa fa-bars"></span>
         </a>
-        <?php echo '<h2><a class="navbar-brand title_text" title="' . get_bloginfo('name') . '" href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a></h2>'; ?>
+        <?php echo '<h2 class="navbar-brand"><a class="title_text" title="' . get_bloginfo('name') . '" href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a></h2>'; ?>
     </div>
 </nav>
