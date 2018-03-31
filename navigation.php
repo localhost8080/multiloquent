3a2bf5d3c1d4cae74e2bd3696df18447aee2f7e0
jonathan <jonathan@adeogroup.co.uk>
2018-03-31 15:25:05 +0100

    navbar-brand

--- navigation.php
+++ navigation.php
@@ -15,6 +15,6 @@
 		<a title="navigation menu" href="javascript:void(0);" class="navbar-brand sidebar-toggle">
 			<span class="fa fa-bars"></span>
 		</a>
-		<?php echo '<h2 class="navbar-brand"><a class="title_text" title="' . get_bloginfo( 'name' ) . '" href="' . esc_url( home_url( '/' ) ) . '">' . get_bloginfo( 'name' ) . '</a></h2>'; ?>
+		<?php echo '<h2 class="navbar-brand"><a class="navbar-brand" title="' . get_bloginfo( 'name' ) . '" href="' . esc_url( home_url( '/' ) ) . '">' . get_bloginfo( 'name' ) . '</a></h2>'; ?>
 	</div>
 </nav>
