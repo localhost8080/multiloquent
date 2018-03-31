ec28e8b46fcedbed77346a87b6d244c37d0f4082
jonathan <jonathan@adeogroup.co.uk>
2018-03-31 14:35:51 +0100

    layout fix

--- breadcrumb.php
+++ breadcrumb.php
@@ -10,7 +10,7 @@
  */
 
 ?>
-<nav class="container-fluid hidden-xs">
+<nav class="container-fluid clearfix hidden-xs">
 	<ul class="breadcrumb clearfix pull-left col-sm-12 col-md-6 col-lg-7" itemscope itemtype="http://schema.org/BreadcrumbList">
 		<?php
 		echo $multiloquent->multiloquent_breadcrumbs();
