921fb4e0e26ed9aa9f1fa897ecd42e115c20caac
jonathan <jonathan@adeogroup.co.uk>
2018-07-28 16:10:18 +0100

    mods

--- social.php
+++ social.php
@@ -9,7 +9,33 @@
  * the social media things
  */
 
+
+$mods = get_theme_mods();
+if (! empty($mods['bootswatch']) && $mods['bootswatch'] == 'mdb') {
+    ?>
+<div class="navbar fixed-bottom navbar-dark bg-primary hidden-xs">
+	<div class="container-fluid clearfix">
+		<div class="col-xs-4 col-sm-7 col-md-6 col-lg-6">
+			<?php
+			// in here goes the jetpack plugin thing
+			if ( function_exists( 'sharing_display' ) ) {
+				echo sharing_display();
+			}
 ?>
+		</div>
+		<div class="col-xs-6 col-sm-5 col-md-6 col-lg-6 sharing_buttons">
+			<?php
+			if ( is_active_sidebar( 8 ) ) {
+				dynamic_sidebar( 8 );
+			}
+?>
+		</div>
+	</div>
+</div>
+<?php
+} else {
+    ?>
+
 <div class="navbar fixed-bottom navbar-light bg-light hidden-xs">
 	<div class="container-fluid clearfix">
 		<div class="col-xs-4 col-sm-7 col-md-6 col-lg-6">
@@ -29,3 +55,7 @@
 		</div>
 	</div>
 </div>
+
+<?php
+}
+?>
