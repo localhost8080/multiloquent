52eab7250ca2b5dc8fdc8ae1b2da3fa0a8b4dd95
jonathan <jonathan@adeogroup.co.uk>
2018-07-28 16:08:28 +0100

    mods

--- navigation.php
+++ navigation.php
@@ -9,12 +9,32 @@
  * nav bar
  */
 
-?>
+$mods = get_theme_mods();
+if (! empty($mods['bootswatch']) && $mods['bootswatch'] == 'mdb') {
+    ?>
+<nav role="navigation" class="navbar fixed-top navbar-dark bg-primary">
+	<div class="">
+		<a title="navigation menu" href="javascript:void(0);" class="navbar-brand sidebar-toggle">
+			<span class="fa fa-bars"></span>
+		</a>
+		<?php echo '<h2 class="navbar-brand"><a class="navbar-brand" title="' . get_bloginfo('name') . '" href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a></h2>'; ?>
+	</div>
+</nav>
+
+<?php
+} else {
+    ?>
+
 <nav role="navigation" class="navbar fixed-top navbar-light bg-light">
 	<div class="">
 		<a title="navigation menu" href="javascript:void(0);" class="navbar-brand sidebar-toggle">
 			<span class="fa fa-bars"></span>
 		</a>
-		<?php echo '<h2 class="navbar-brand"><a class="navbar-brand" title="' . get_bloginfo( 'name' ) . '" href="' . esc_url( home_url( '/' ) ) . '">' . get_bloginfo( 'name' ) . '</a></h2>'; ?>
+		<?php echo '<h2 class="navbar-brand"><a class="navbar-brand" title="' . get_bloginfo('name') . '" href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a></h2>'; ?>
 	</div>
 </nav>
+
+
+<?php
+}
+?>
