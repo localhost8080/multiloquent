07deeb563314f7f7d42f842886f732ee9c7b24ec
jonathan <jonathan@adeogroup.co.uk>
2018-09-15 15:34:04 +0100

    mods

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php
 	if ( function_exists( 'yoast_analytics' ) ) {
