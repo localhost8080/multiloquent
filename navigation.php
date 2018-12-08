576fc483d038b7025e7277eb924f991cbb29f48a
jonathan <jonathan@adeogroup.co.uk>
2018-12-08 12:53:46 +0000

    fix mobile nav

--- navigation.php
+++ navigation.php
@@ -17,7 +17,7 @@ if (! empty($mods['bootswatch']) && $mods['bootswatch'] == 'mdb') {
 		<a title="navigation menu" href="javascript:void(0);" class="navbar-brand sidebar-toggle">
 			<span class="fa fa-bars"></span>
 		</a>
-		<?php echo '<h2 class="navbar-brand"><a class="navbar-brand" title="' . get_bloginfo('name') . '" href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a></h2>'; ?>
+		<?php echo '<h2 class="navbar-brand d-none d-sm-inline"><a class="navbar-brand" title="' . get_bloginfo('name') . '" href="' . esc_url(home_url('/')) . '">' . get_bloginfo('name') . '</a></h2>'; ?>
 	</div>
 	<div class="material-search">
 	<?php get_search_form();?>
