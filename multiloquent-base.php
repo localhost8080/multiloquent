b472071aa161c06824cadae1df09d89ed7362f0e
Born Creative <hi@born-creative.co.uk>
2026-03-17 23:30:42 +0000

    bump version

--- multiloquent-base.php
+++ multiloquent-base.php
@@ -20,7 +20,7 @@ class MultiloquentBase
 
 	public function multiloquent_version(): string
 	{
-		return '26.0.0';
+		return '26.1.0';
 	}
 
 	// -------------------------------------------------------------------------
