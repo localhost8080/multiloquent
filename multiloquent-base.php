37ea72bb31767310495c079ddf936176de4da526
Born Creative <hi@born-creative.co.uk>
2026-03-18 23:54:01 +0000

    version

--- multiloquent-base.php
+++ multiloquent-base.php
@@ -20,7 +20,7 @@ class MultiloquentBase
 
 	public function multiloquent_version(): string
 	{
-		return '26.1.5';
+		return '26.1.6';
 	}
 
 	// -------------------------------------------------------------------------
