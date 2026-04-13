a81b12569c351d4accf177f76bbb14d1d069a069
localhost8080 <localhost8080@gmail.com>
2026-04-13 22:55:46 +0100

    fixes

--- multiloquent-base.php
+++ multiloquent-base.php
@@ -21,7 +21,7 @@ class MultiloquentBase
 
 	public function multiloquent_version(): string
 	{
-		return '26.1.7';
+		return '26.1.8';
 	}
 
 	// -------------------------------------------------------------------------
